1ddc4b3c8ca8a7da3dcfe95f32c72792ea2e7fa6
sean <yihai8721@vip.qq.com>
2017-08-07 19:03:58 +0800

    no message

---
+++ src/views/apartment/billManagement.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!--<menu-box></menu-box>
+    <div class="right-content" id="right-content">
+      <right-header></right-header>-->
+      <div class="wordbench-box">
+        这是账单管理的页面
+      </div>
+     <!-- <footer-box></footer-box>
+    </div>-->
+  </div>
+</template>
+
+<script>
+
+  import menuBox from '../../components/menuBox.vue';
+  import  rightHeader from '../../components/rightHeader.vue';
+  import  footerBox from '../../components/footerBox.vue';
+
+  export default {
+    components: {
+      rightHeader,
+      menuBox,
+      footerBox
+    },
+    data() {
+      return{
+
+      }
+    },
+    methods: {
+
+    }
+  }
+
+</script>
